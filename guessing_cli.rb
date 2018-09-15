a741cdb00e58aa9b425a624843939011f00c830e
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 21:14:55 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,12 +3,13 @@ def run_guessing_game
   user_guess = gets.chomp.to_s
   computer_guess = rand(1..6)
 
-  if user_guess == computer_guess
+  if user_guess == "exit"
+      puts "Goodbye!"
+    elsif user_guess == computer_guess
     return "You guessed the correct number!"
-  elsif user_guess != computer_guess
+    elsif user_guess != computer_guess
     return "The computer guessed #{computer_guess}"
-  elsif user_guess == 'exit'
-    puts "Goodbye!"
+
   else
     puts "Invalid input"
   end
